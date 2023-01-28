6f69c5d76e17795486cae96c326946c9cf366e67
IRod22 <ivan18rod@yahoo.com>
2023-01-28 14:19:34 -0600

    feat: add data

--- src/app/relations.service.ts
+++ src/app/relations.service.ts
@@ -7,7 +7,7 @@ export type Node = {
   name: string,
   description: string,
   twitter: Maybe<string>,
-  yt: Maybe<string>,
+  yt: Maybe<{ id: string, short: boolean}>,
   website: Maybe<string>,
 }
 
@@ -21,7 +21,69 @@ export type Edge = {
   providedIn: 'root'
 })
 export class RelationsService {
-  nodes: Node[] = []
-  edges: Edge[] = []
+  nodes: Node[] = [
+    {
+      id: 'yt',
+      name: 'YouTube',
+      description: 'a platform for sharing and streaming videos.',
+      twitter: Just('YouTube'),
+      yt: None(),
+      website: Just('https://youtube.com'),
+    },
+    {
+      id: 'crashcourse',
+      name: 'Crash Course',
+      description: 'a show and YouTube channel for educational content used by highschool and college teachers and students.',
+      twitter: Just('TheCrashCourse'),
+      yt: Just({id: 'crashcourse', short: true}),
+      website: Just('https://thecrashcourse.com/'),
+    },
+    {
+      id: 'rll',
+      name: 'RealLifeLore',
+      description: 'a channel where several topics are explained by using economics, history, geography, science, and "extreme speculation."',
+      twitter: Just('RealLifeLore22'),
+      yt: Just({id: 'RealLifeLore', short: true}),
+      website: None(),
+    },
+    {
+      id: 'wendoverpro',
+      name: 'Wendover Production',
+      description: 'is about economics and aviation and explains several topics using economics, history, geography, and science.',
+      twitter: Just('wendoverpro'),
+      yt: Just({id:'Wendoverproductions', short: true}),
+      website: Just('https://www.wendoverproductions.com/'),
+    },
+    {
+      id: 'polymatter',
+      name: 'PolyMatter',
+      description: 'is about explaining several topics using economics, history, geography, and science.',
+      twitter: Just('Polymatters'),
+      yt: Just({id:'UCgNg3vwj3xt7QOrcIDaHdFg', short: false}),
+      website: Just('https://www.polymatter.yt'),
+    },
+  ]
+  edges: Edge[] = [
+    {
+      node1: 'crashcourse',
+      name: OneWay('hosted on', 'channels'),
+      node2: 'yt',
+    },
+    {
+      node1: 'rll',
+      name: OneWay('hosted on', 'channels'),
+      node2: 'yt',
+    },
+    {
+      node1: 'wendoverpro',
+      name: OneWay('hosted on', 'channels'),
+      node2: 'yt',
+    },
+    {
+      node1: 'polymatter',
+      name: OneWay('hosted on', 'channels'),
+      node2: 'yt',
+    },
+  ]
   constructor() { }
 }
