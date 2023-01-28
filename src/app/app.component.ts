888173cf06eb0a37cd9c4173e428fdf87aab6d97
IRod22 <ivan18rod@yahoo.com>
2023-01-28 15:51:41 -0600

    feat: update main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RelationsService } from './relations.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'ed-yt-relations';
+  profiles = this.relationsService.nodes
+  constructor(public relationsService: RelationsService) { }
 }
